8cd109a9f2f1ed43bc951d48060f82dd571cf8ee
Leanne Gass <leannegass@gmail.com>
2023-05-31 12:09:58 +0100

    typescript function

--- src/Message.tsx
+++ src/Message.tsx
@@ -2,9 +2,16 @@ import React from 'react';
 
 export const Message = (prop:{data : string}):any => {
 
+    function greeter(name : string): any {
+        return (
+            <p>Hi {name} </p>
+        )
+    }
+
     return (
         <>
           <h1> {prop.data} </h1>
+          <h2>{greeter('leanne')} </h2>
 
         </>
     )
