f6c2dcdd4f3cd259939ebad1a1d35d9ce96a3494
Leanne Gass <leannegass@gmail.com>
2023-05-31 13:43:02 +0100

    optional and rest parameters

--- src/Message.tsx
+++ src/Message.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 
 export const Message = (prop:{data : string}):any => {
 
-    function greeter(name : string): any {
+    function greeter(name : string, age ?: number, ...skills : string[]): any {
         return (
-            <p>Hi {name} </p>
+            <p>Hi {name +" " + skills.join(',')} </p>
         )
     }
 
     return (
         <>
           <h1> {prop.data} </h1>
-          <h2>{greeter('leanne')} </h2>
+          <h2>{greeter('leanne',55,'Java','Javascript')} </h2>
 
         </>
     )
