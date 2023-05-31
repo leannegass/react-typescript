613ae99ad30af248f66f19667323d58a8bcd8895
Leanne Gass <leannegass@gmail.com>
2023-05-31 15:41:39 +0100

    interfaces

--- src/Message.tsx
+++ src/Message.tsx
@@ -15,7 +15,26 @@ export const Message = (prop:{data : string}):any => {
     function add(a : string, b : string) : string {
         return <> {a + b} </>
     }
-    
+    //interface is similar to a class but it does not create objects, just a 
+    interface IsPerson {
+        name : string;
+        age : number; //properties
+        greet(a: string): string;//method returns string
+        height(a: number) : void;//method that returns void
+
+    }
+    const me: IsPerson = {
+        name : 'leanne',
+        age : 50,
+        greet(text : string): string{
+            return (<> Hello {text}</>)
+        },  
+        height(cms : number): void{
+            console.log("height is "+ cms)
+        }
+
+    }
+    // console.log(me)
 
 
     return (
@@ -24,6 +43,8 @@ export const Message = (prop:{data : string}):any => {
           <h2>{greeter('leanne',55,'Java','Javascript')} </h2>
           <p>{add(1,6)}</p>
           <p>{add('hi', 'Juno')} </p>
+          <p>{me.greet('leanne')}</p>
+          <p>{me.height(170)}</p>
 
         </>
     )
